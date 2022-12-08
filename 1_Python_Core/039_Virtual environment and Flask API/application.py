841dc513af72681da842c2ee1ea6c47ab65fb3e8
Himanshu Ramchandani <himanshuramchandani08@gmail.com>
2022-12-08 20:34:07 +0530

    day 39 virtual env and api basics

---
+++ 1_Python_Core/039_Virtual environment and Flask API/application.py
@@ -0,0 +1,12 @@
+#  API - Application Programming Interface
+
+# API endpoint - URL
+
+# https://www.google.co.in/search?q=python
+# scheme     DNS            route  query-string
+#            IP- IPv4 IPv6
+
+# http
+# https
+# ftp
+# file
